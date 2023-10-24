2692b696b0944c4710a29e7f40793085c6525cfe
Gabriel Zapata <gabrielzp@protonmail.com>
2023-10-24 16:33:06 +0100

    add users to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_165934) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_130533) do
   create_table "event_tags", force: :cascade do |t|
     t.integer "event_id", null: false
     t.integer "tag_id", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_165934) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_events", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_165934) do
 
   add_foreign_key "event_tags", "events"
   add_foreign_key "event_tags", "tags"
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
 end
